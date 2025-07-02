c30bf33763ac23ef191bb137c40b325b49df749a
ryanhunter <ryanhunter30479@outlook.com>
2025-07-02 21:34:14 +0800

    update init rsim version

--- chains/build/buildflow.rb
+++ chains/build/buildflow.rb
@@ -7,35 +7,35 @@ generator :node_load do
 	arg :debug,:value=>'--debug ${debug}', :required => false
 	phase 0
 end
-generator :rtl_build do
+generator :file_build do
 	# generate to link all nodes compatibale with IP-XACT and build to xml baed meta data
-	exe "#{ENV['RSIM_HOME']}/bins/rtl_build"
+	exe "#{ENV['RSIM_HOME']}/bins/builder"
 	arg :config,:value=>'--config ${config}', :required => true
-	arg :db_dir,:value=>'--db ${db_dir}', :required => true
 	arg :out,:value=>'--out ${out}', :required => true
+#	arg :builder,:value=>'--rtl', :required => true
+	arg :method,:value=>'--method link', :required => true
 	phase 1
 end
-generator :dv_build do
-	#generator to build dv files according to the config in db.
-	exe "#{ENV['RSIM_HOME']}/bins/dv_build"
-	arg :db_dir,:value=>'--db ${db_dir}', :required => true
-	arg :out,:value=>'--out ${out}', :required => true
-	phase 1
-end
+#generator :dv_build do
+#	#generator to build dv files according to the config in db.
+#	exe "#{ENV['RSIM_HOME']}/bins/builder"
+#	arg :config,:value=>'--config ${config}', :required => true
+#	arg :out,:value=>'--out ${out}', :required => true
+#	arg :builder,:value=>'--dv', :required => true
+#	arg :method,:value=>'--method link', :required => true
+#	phase 1
+#end
 chain :build do
 	generator :node_load do
-		param :out => ENV['OUT_HOME']
-		param :entry => ENV['RSIM_ENTRY']
-		param :verbosity => 10
-		param :debug => 10
+		param '--out'=>ENV['OUT_HOME']
+		param '--entry'=>ENV['RSIM_ENTRY']
+		param '--verbosity'=>10
+		param '--debug'=>10
 	end
-	#generator :rtl_build do
-	#	#param :config=>@params[:config]
-	#	param :db_dir=>File.join(ENV['OUT_HOME'],'meta/')
-	#	param :out=>ENV['OUT_HOME']
-	#end
+	generator :file_build do
+		param '--out'=>ENV['OUT_HOME']
+	end
 	#generator :dv_build do
-	#	param :db_dir=>File.join(ENV['OUT_HOME'],'meta/')
-	#	param :out=>ENV['OUT_HOME']
+	#	param '--out'=>ENV['OUT_HOME']
 	#end
 end
